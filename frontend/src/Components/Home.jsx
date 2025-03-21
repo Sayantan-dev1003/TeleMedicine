8aa5179d5f1c1f5962c7f33c427c8c099e4dc8e3
Sayantan Halder <sayantanhalder78@gmail.com>
2025-03-22 01:39:28 +0530

    Hero Section done

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import LandingImage from "../../public/Landingimg.jpg";
 import Navbar from './Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const LandingPage = () => {
     return (
         <>
             <Navbar />
-            <div className="w-full min-h-screen bg-cover bg-center backdrop-blur-2xl brightness-90 flex justify-center items-center" style={{
-                backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.7)), url(${LandingImage})`
-            }}>
-                <div className='items-center flex flex-col gap-5 text-center w-2/3 relative top-10'>
-                    <h1 className='font-bold text-7xl poppins text-white ' style={{ textShadow: "3px 3px 5px rgba(0, 0, 0, 0.8)" }}>Telemedicine</h1>
-                    <p className='font-bold text-4xl monterrat text-[#C9E9D2]' style={{ textShadow: "3px 3px 5px rgba(0, 0, 0, 0.8)" }}>Quality Healthcare, Anytime, Anywhere!</p>
-                    <p className='font-semibold text-3xl  text-[#C1D8C3] ' style={{ textShadow: "3px 3px 5px rgba(0, 0, 0, 0.8)" }}>Connect with top doctors online for consultations, prescriptions, and expert medical advice from the comfort of your home.</p>
+            <div 
+                className="w-full min-h-screen bg-no-repeat bg-cover bg-center flex justify-end p-8 items-center poppins" 
+                style={{
+                    backgroundImage: `
+                        linear-gradient(to right, rgba(0, 0, 0, 0.5) 30%, rgba(0, 0, 0, 0.9) 70%),
+                        url(${LandingImage})
+                    `,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'top left'
+                }}
+            >
+                <div className='flex flex-col gap-5 text-right w-[55%] relative justify-center items-end top-10'>
+                    <h1 className='font-bold text-7xl poppins text-[#81dede] montserrat' 
+                        style={{ textShadow: "3px 3px 5px rgba(0, 0, 0, 0.8)" }}>
+                        TeleMedicine
+                    </h1>
+                    <p className='font-bold text-2xl montserrat text-white' 
+                        style={{ textShadow: "3px 3px 5px rgba(0, 0, 0, 0.8)" }}>
+                        Quality Healthcare, Anytime, Anywhere!
+                    </p>
+                    <p className='font-semibold text-lg text-white' 
+                        style={{ textShadow: "3px 3px 5px rgba(0, 0, 0, 0.8)" }}>
+                        Connect with top doctors online for consultations, prescriptions, and expert medical advice from the comfort of your home.
+                    </p>
 
-                    <button className='bg-[#0c6f6f] text-white p-6 text-center text-xl rounded-full cursor-pointer poppins hover:bg-yellow-300 hover:text-[#064747] hover:scale-110 transform duration-300 ease-in-out font-bold'>Book an appointment</button>
+                    <button className='bg-[#027c7c] text-white mt-5 w-52 px-6 py-3 tracking-wider text-center text-xl rounded-full cursor-pointer poppins hover:bg-[#81dede] hover:text-[#064747] hover:scale-110 transform duration-300 ease-in-out font-bold'>
+                        Get Started <FontAwesomeIcon icon={faArrowRight} />
+                    </button>
                 </div>
-
             </div>
         </>
     );
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -10,24 +10,24 @@ const Navbar = () => {
     const [isSigninOpen, setIsSigninOpen] = useState(false);
 
     return (
-        <nav className="w-full fixed z-10 bg-[#064848] py-3 px-6 poppins">
+        <nav className="w-full fixed z-10 bg-[#064848] py-3 px-8 poppins">
             <div className="w-full flex justify-between items-center">
                 <div className="flex items-center gap-3">
                     <FontAwesomeIcon icon={faHeart} className='fa-solid fa-heart fa-beat text-white text-3xl' />
-                    <div className="text-white text-2xl font-bold montserrat">Telemedicine</div>
+                    <div className="text-white text-2xl font-bold montserrat">TeleMedicine</div>
                 </div>
                 <ul className="flex space-x-3">
                     <li
-                        className="text-white flex gap-2 items-center hover:text-yellow-200  cursor-pointer text-xl px-4 py-2 rounded-full hover:bg-[#0d3733] hover:scale-110 transform duration-300 ease-in-out"> <FontAwesomeIcon icon={faHome} className='fa-solid fa-beat text-white text-xl ' />Home
+                        className="text-white flex gap-2 items-center hover:text-[#81dede]  cursor-pointer text-xl px-4 py-2 rounded-full hover:bg-[#0d3733] hover:scale-105 transform duration-300 ease-in-out"> <FontAwesomeIcon icon={faHome} className='fa-solid fa-beat text-white text-xl ' />Home
                     </li>
                     <li
-                        className="text-white flex gap-2 items-center  hover:text-yellow-200 cursor-pointer text-xl px-4 py-2 rounded-full hover:bg-[#0d3733] hover:scale-110 transform duration-300 ease-in-out"><FontAwesomeIcon icon={faCalendarCheck} className='fa-solid fa-beat text-white text-xl ' />Book Appointment
+                        className="text-white flex gap-2 items-center  hover:text-[#81dede] cursor-pointer text-xl px-4 py-2 rounded-full hover:bg-[#0d3733] hover:scale-105 transform duration-300 ease-in-out"><FontAwesomeIcon icon={faCalendarCheck} className='fa-solid fa-beat text-white text-xl ' />Book Appointment
                     </li>
                     <li
-                        className="text-white flex gap-2 items-center hover:text-yellow-200 cursor-pointer text-xl px-4 py-2 rounded-full hover:bg-[#0d3733] hover:scale-110 transform duration-300 ease-in-out"><FontAwesomeIcon icon={faVideo} className='fa-solid fa-beat text-white text-xl ' />Video Consultation
+                        className="text-white flex gap-2 items-center hover:text-[#81dede] cursor-pointer text-xl px-4 py-2 rounded-full hover:bg-[#0d3733] hover:scale-105 transform duration-300 ease-in-out"><FontAwesomeIcon icon={faVideo} className='fa-solid fa-beat text-white text-xl ' />Video Consultation
                     </li>
                     <button
-                        className="text-[#0d3733] bg-white flex gap-2 items-center hover:text-yellow-200 cursor-pointer text-lg px-4 py-2 rounded-full hover:bg-[#0d3733] hover:scale-110 transform duration-300 ease-in-out font-bold" onClick={() => setIsSignupOpen(true)}><FontAwesomeIcon icon={faUserPlus} className='fa-solid fa-beat text-lg ' />SignUp
+                        className="text-[#0d3733] bg-white flex gap-2 items-center hover:text-[#81dede] cursor-pointer text-lg px-4 py-2 rounded-full hover:bg-[#0d3733] hover:scale-105 transform duration-300 ease-in-out font-bold" onClick={() => setIsSignupOpen(true)}><FontAwesomeIcon icon={faUserPlus} className='fa-solid fa-beat text-lg ' />SignUp
                     </button>
 
                 </ul>
